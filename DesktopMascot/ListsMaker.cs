f8b959ee5854dd52114918be15f5a773d9f32ea3
NishiharaUme <nishihara@cps.im.dendai.ac.jp>
2018-05-25 19:53:38 +0900

    add ListsMaker

---
+++ DesktopMascot/ListsMaker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopMascot
+{
+    public abstract class ListsMaker
+    {
+        protected List<MainScreenMgr> mainScreenMgrList;
+        protected List<SubScreenMgr> subScreenMgrList;
+        protected List<CharacterSet> characterSetList;
+        protected List<IDAndName> mainScreenMgrIdAndNameList;
+        protected List<IDAndName> characterSetIdAndNameList;
+        protected Form controller;
+
+        public ListsMaker(Controller controller)
+        {
+            this.mainScreenMgrList = null;
+            this.subScreenMgrList = null;
+            this.characterSetList = null;
+            this.mainScreenMgrIdAndNameList = null;
+            this.characterSetIdAndNameList = null;
+            this.controller = controller;
+        }
+
+        public List<MainScreenMgr> getMainScreenMgrList()
+        {
+            if(mainScreenMgrList != null)
+            {
+                return mainScreenMgrList;
+            }
+            return null;
+        }
+        public List<SubScreenMgr> getSubScreenMgrList()
+        {
+            if (subScreenMgrList != null)
+            {
+                return subScreenMgrList;
+            }
+            return null;
+        }
+        public List<CharacterSet> getCharacterSetList()
+        {
+            if (characterSetList != null)
+            {
+                return characterSetList;
+            }
+            return null;
+        }
+        public List<IDAndName> getMainScreenMgrIDAndNameList()
+        {
+            if (mainScreenMgrIdAndNameList != null)
+            {
+                return mainScreenMgrIdAndNameList;
+            }
+            return null;
+        }
+        public List<IDAndName> getCharacterSetIDAndNameList()
+        {
+            if (characterSetIdAndNameList != null)
+            {
+                return characterSetIdAndNameList;
+            }
+            return null;
+        }
+    }
+}
---
+++ DesktopMascot/TestListsMaker.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DesktopMascot
+{
+    public class TestListsMaker : ListsMaker
+    {
+        public TestListsMaker(Controller controller) : base(controller)
+        {
+            // メンバ変数の初期化
+            mainScreenMgrList = new List<MainScreenMgr>();
+            subScreenMgrList = new List<SubScreenMgr>();
+            characterSetList = new List<CharacterSet>();
+            mainScreenMgrIdAndNameList = new List<IDAndName>();
+            characterSetIdAndNameList = new List<IDAndName>();
+
+            // 一時的に確保しておく変数
+            MainScreenMgr bufMainScrMgr;
+            SubScreenMgr bufSubScrMgr;
+            CharacterSet bufCharaSet;
+            IDAndName bufIDAndName;
+
+            // 全てのCharacterSetを生成し、Listに入れる
+            bufCharaSet = new TestCharacterSet(0);
+            characterSetList.Add(bufCharaSet);
+            // IDと名前も同時に記録しておく
+            bufIDAndName.id = bufCharaSet.getId();
+            bufIDAndName.name = bufCharaSet.getName();
+            characterSetIdAndNameList.Add(bufIDAndName);
+
+
+            // 全てのSubScreenMgrを生成し、Listに入れる
+            // コンストラクタのID番号は取りあえず0で設定している
+            bufSubScrMgr = new TestSubScreen(0);
+            subScreenMgrList.Add(bufSubScrMgr);
+            bufSubScrMgr = new TestSubScreen2(1);
+            subScreenMgrList.Add(bufSubScrMgr);
+
+
+            // 全てのMainScreenMgrを生成し、Listに入れる
+            // コンストラクタのID番号は取りあえず0で設定している
+            bufMainScrMgr = new TestMainScreen(0, subScreenMgrList, controller);
+            mainScreenMgrList.Add(bufMainScrMgr);
+            // IDと名前も同時に記録しておく
+            bufIDAndName.id = bufMainScrMgr.getId();
+            bufIDAndName.name = bufMainScrMgr.getName();
+            mainScreenMgrIdAndNameList.Add(bufIDAndName);
+
+
+
+            // MainScreenMgrとSubScreenMgrが利用するデフォルトキャラクターを設定する
+            // 使用されるスクリーン全てにキャラクターが設定されるようにする
+
+            // MainScreenMgrのデフォルトキャラクター設定
+            foreach (MainScreenMgr item in mainScreenMgrList)
+            {
+                item.setCharacterSet(characterSetList[0]);
+            }
+
+            // SubScreenMgrのデフォルトキャラクター設定
+            foreach (SubScreenMgr item in subScreenMgrList)
+            {
+                item.setCharacterSet(characterSetList[0]);
+            }
+        }
+
+    }
+}
